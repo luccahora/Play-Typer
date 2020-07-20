9d2f9f064d01aaf9711cdfd39f19853087636fd1
Lucca Hora <luccahora@gmail.com>
2020-07-20 18:10:35 -0300

    Buscando quantidade de palavras

--- play-typer/public/js/main.js
+++ play-typer/public/js/main.js
@@ -1,2 +1,8 @@
-var frase = $(".frase");
-console.log(frase);
+var frase = $(".frase").text()
+var numeroPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+
+tamanhoFrase.text(numeroPalavras);
+
+
+
