56870a1ebd87c46043e8c8be808c5ec15b8412f7
Lucca Hora <luccahora@gmail.com>
2020-07-20 20:04:13 -0300

    Contador de palavras e caracteres

--- play-typer/public/js/main.js
+++ play-typer/public/js/main.js
@@ -1,8 +1,20 @@
 var frase = $(".frase").text()
 var numeroPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
-
 tamanhoFrase.text(numeroPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function () {
+    var conteudo = campo.val();
+
+    var quantidadePalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(quantidadePalavras);
+
+    var quantidadeCaracteres = conteudo.length;
+    $("#contador-caracteres").text(quantidadeCaracteres);
+
+
+})
 
 
 
